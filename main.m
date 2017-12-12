44c7475059ec17057e10389dfdd83f9d77cd94ca
pat310 <patrick.trasborg@gmail.com>
2017-12-11 22:08:43 -0500

    working code to predict number of guests attending

--- main.m
+++ main.m
@@ -2,9 +2,36 @@ clear;
 close all;
 clc;
 
+% number of simulations
+n = 1000;
+
+% load guest list with associated probabilities of attendance
 data = csvread('guestList.csv');
 X = data(:, 3);
 
+% Plot the probability distribution of the guests
+figure(1);
 hist(X, 20);
 xlabel('Probability');
-ylabel('Group size');
+ylabel('Number of guests');
+
+% number of guests
+num_guests = length(X);
+
+% determine the number of times each guest will attend using random binomial distribution
+guest_attending = zeros(n, num_guests);
+
+for i = 1:num_guests
+  guest_attending(:, i) = binornd(1, X(i), n, 1);
+end
+
+% sum the number of guests attending in each simulation
+summed_simulations = sum(guest_attending, 2);
+
+% group simulation counts into bins (i.e. how many times each count happens)
+[nn, xx] = hist(summed_simulations);
+
+figure(2);
+bar(xx, nn ./ n);
+xlabel('Attendees');
+ylabel('Probability')
